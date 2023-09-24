1b95a09a881c396db21235ff0d9b759f13dd2d43
AdrianMalaescu <adrianmalaescuu@gmail.com>
2023-09-24 15:55:29 +0300

    116 to 119 (40 min of course = 2 actual hours)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-const weekdays = ['mon', 'tue','wed', 'thu', 'fri', 'sat', 'sun'];
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 const openingHours = {
   [weekdays[3]]: {
     open: 12,
@@ -22,11 +22,11 @@ const openingHours = {
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   // ES6 enhanced object literals
   openingHours,
@@ -36,12 +36,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
     console.log(`Order received ${this.starterMenu[starterIndex]} and this ${this.mainMenu[mainIndex]}
     will be delivered to ${address} at ${time}`);
   },
@@ -58,25 +53,76 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+rest.set(2, "Lisbon, Portugal");
 
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "we are open")
+  .set(false, "we are closed");
 
- const rest = new Map ();
- rest.set('name', 'Classico Italiano');
- rest.set(1, 'Firenze, Italy');
- rest.set(2, 'Lisbon, Portugal');
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
- rest
- .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
- .set('open', 11)
- .set('close', 23)
- .set(true, 'we are open')
- .set(false, 'we are closed')
+console.log(rest);
 
- console.log(rest.get('name'));
- console.log(rest.get(true));
- console.log(rest.get(1));
+const time = 8;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
 
- console.log(rest);
+console.log(rest.has("categories"));
+rest.delete(2);
+
+const arr = [1, 2];
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+const question = new Map([
+  ["question", "What is the best progreaming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct (bine boss)"],
+  [false, "Mai trage o fisă"],
+]);
+
+console.log(question);
+
+// ------------------------------  Convert object to map --------------------------
+
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+
+// --------------------------- Test quiz -----------------------------
+
+// const answer = Number(prompt("Zi sefule"));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get("correct") === answer));
+
+// ----------------------------- Convert map to array ---------------------------
+
+console.log([...question]);
+console.log(question.entries());
+console.log(question.keys());
+console.log(question.values());
+console.log([...question.keys()]);
+console.log([...question.values()]);
 
 /*
 
@@ -142,10 +188,8 @@ if (users.length > 0) console.log(users[0].name); else console.log('user array e
 
 */
 
-
-
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-// for (const item of menu) 
+// for (const item of menu)
 // console.log(item);
 
 // for (const [i, el] of menu.entries()){
@@ -166,36 +210,25 @@ if (users.length > 0) console.log(users[0].name); else console.log('user array e
 //     owner: 'Giovanni Rossi',
 //   };
 
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
 
-  // OR assignment operator
-  // rest1.numGuests = rest1.numGuests || 10;
-  // rest2.numGuests = rest2.numGuests || 10;
-  // rest1.numGuests ||= 10;
-  // rest2.numGuests ||= 10;
+//nullish assignment operator (null or undefined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
+//AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>'
+// rest2.owner = rest2.owner && '<ANONYMOUS>'
 
-  //nullish assignment operator (null or undefined)
-  // rest1.numGuests ??= 10;
-  // rest2.numGuests ??= 10;
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
 
-
-  //AND assignment operator
-  // rest1.owner = rest1.owner && '<ANONYMOUS>'
-  // rest2.owner = rest2.owner && '<ANONYMOUS>'
-  
-  // rest1.owner &&= '<ANONYMOUS>';
-  // rest2.owner &&= '<ANONYMOUS>';
-
-  // console.log(rest1);
-  // console.log(rest2);
-
-
-
-
-
-
-
-
+// console.log(rest1);
+// console.log(rest2);
 
 // Use ANY data type, return ANY data type, short-circuiting
 
@@ -207,7 +240,6 @@ if (users.length > 0) console.log(users[0].name); else console.log('user array e
 // console.log(undefined || null);
 
 // console.log(undefined || '' || 'Hello' || 23 || null );
-
 
 // Faulsy values
 // let restaurantnumGuests = 0;
@@ -234,7 +266,6 @@ if (users.length > 0) console.log(users[0].name); else console.log('user array e
 //   restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 // 1) Destructuring
-
 
 // SPREAD, beacause on RIGHT side of =
 // const arr = [1, 2, ...[3, 4]];
@@ -423,7 +454,6 @@ console.log(restaurant.name);
 
 */
 
-
 /*
 
 Data Structures, Modern Operators and Strings
@@ -467,50 +497,48 @@ GOOD LUCK �
 
 */
 
-
 const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
   players: [
-  [
-  'Neuer',
-  'Pavard',
-  'Martinez',
-  'Alaba',
-  'Davies',
-  'Kimmich',
-  'Goretzka',
-  'Coman',
-  'Muller',
-  'Gnarby',
-  'Lewandowski',
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
   ],
-  [
-  'Burki',
-  'Schulz',
-  'Hummels',
-  'Akanji',
-  'Hakimi',
-  'Weigl',
-  'Witsel',
-  'Hazard',
-  'Brandt',
-  'Sancho',
-  'Gotze',
-  ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
-  'Hummels'],
-  date: 'Nov 9th, 2037',
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
   odds: {
-  team1: 1.33,
-  x: 3.25,
-  team2: 6.5,
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
-  }
+};
 
-  /*
+/*
 
   // ---------------- 1 ------------------------
 
@@ -695,7 +723,6 @@ for (const player of game.scored) {
 
 
 */
-
 
 /*
 //Property names
