66b8ea2cff6859b52e31c7ce5228a441195d5557
AdrianMalaescu <adrianmalaescuu@gmail.com>
2024-06-28 13:57:49 +0300

    2H Learning | 137 - 140 | GT Sectiunea 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -136,7 +136,7 @@ greet2("Hello")("Jonas");
 greet3("Hello")("Jonas");
 
 */
-
+/* 
 // Lesson 133
 const lufthansa = {
   airline: "lufthansa",
@@ -249,7 +249,7 @@ console.log(addVAT2(23));
 
 // Coding Challenge #1
 
-/* 
+
 Let's build a simple poll app!
 
 A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
@@ -278,7 +278,7 @@ BONUS TEST DATA 1: [5, 2, 3]
 BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
-*/
+
 
 const poll = {
   question: "What is your favourite programming language?",
@@ -324,3 +324,126 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 // [5, 2, 3]
 // [1, 5, 3, 9, 6, 1]
+
+
+
+
+const run0nce = function () {
+  console.log("This will never run again");
+};
+
+run0nce();
+
+// IIFE - Imediatly Invoked Function Expressions IIFE
+
+(function () {
+  console.log("This will never run again");
+  const isPrivate = 23;
+})();
+
+(() => console.log("This will ALSO never run again"))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
+
+
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+
+//Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+// Assinging f function to G
+g();
+f();
+
+// Re-assugbubg f Function to H
+
+h();
+f();
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait}`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
+
+/*
+const makeitblue = header().addEventListener(
+  "click",
+  (h1.style.color = "blue")
+);
+
+*/
+
+(function () {
+  header().addEventListener("click", (header.style.color = "blue"));
+})();
